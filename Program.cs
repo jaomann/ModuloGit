9eda3ac370ccf8e940cb17b2ff73e47c4acf718b
João Emanuel <joaoeemanuel18@gmail.com>
2022-12-09 19:28:37 -0300

    Nexus Region

--- Program.cs
+++ Program.cs
@@ -6,20 +6,11 @@ namespace ModuloGit
     {
         static void Main(string[] args)
         {
-            do
-            {
-                Console.WriteLine("Digite duas coordenadas(X e Y)");
-                int x = int.Parse(Console.ReadLine());
-                int y = int.Parse(Console.ReadLine());
-                if (x > 0) && ()
-                {
-                    Console.WriteLine("Quadrante 1");
+            #region ??
+            Console.WriteLine("sai loko");
+            Console.WriteLine("Faz uma farofa");
+                #endregion
 
-                };
-                    
-
-            }
-            while (x != 0) || (y != 0);
         }
     }
 }
