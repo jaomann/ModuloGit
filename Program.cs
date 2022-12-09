0b7faa76bafbb7fc40a069d1317aedc99e76ae2f
João Emanuel <joaoeemanuel18@gmail.com>
2022-12-09 19:13:37 -0300

    Iniciando Projeto

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ModuloGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Digite duas coordenadas(X e Y)");
+                int x = int.Parse(Console.ReadLine());
+                int y = int.Parse(Console.ReadLine());
+                if (x > 0) && ()
+                {
+                    Console.WriteLine("Quadrante 1");
+
+                };
+                    
+
+            }
+            while (x != 0) || (y != 0);
+        }
+    }
+}
